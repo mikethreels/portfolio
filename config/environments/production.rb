a42a53f5d3876ddaa52ced6f77b173b4422eb246
mikethreels <michaelthreels@gmail.com>
2020-08-21 01:47:39 +0200

    fixing the redirect heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "example.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+  }
+
+  config.action_mailer.default_url_options = { host: "https://dashboard.heroku.com/apps/fathomless-plains-56756"}
+
+
 end
