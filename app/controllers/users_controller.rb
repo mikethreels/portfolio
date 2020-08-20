3d20839333ffaa75d159a4f046247420837e447e
mikethreels <michaelthreels@gmail.com>
2020-08-21 01:03:01 +0200

    redirection after form submission changed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,9 +28,9 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to request.referrer, notice: 'User was successfully created.' }
+        format.html { redirect_to request.referrer, notice: 'success fully send.' }
       else
-        format.html { render :new }
+        format.html { redirect_to request.referrer}
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -42,8 +42,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+        format.html { redirect_to request.referrer, notice: 'User was successfully updated.' }
       else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
